11a3471a4a6a81f38e8ce1c5a6a82fd827da0916
Denis Balabanov <balabanov.ds@gmail.com>
2024-01-24 13:51:49 +0300

    upd

--- hw03/repo.h
+++ hw03/repo.h
@@ -1,7 +1,15 @@
 #include <fstream>
+#include <unordered_map>
 
 // store result to file.
-bool store_result(std::ofstream &file, std::string user, int value);
+bool store_one(std::ofstream &file, const std::string user, const int value);
 
 // print all results.
 bool print_result_table(std::ifstream &file);
+
+// get best results from db.
+std::unordered_map<std::string, int> get_best(std::ifstream &file);
+
+// rewrite db data.
+bool recreate_results(std::ofstream &file,
+                      std::unordered_map<std::string, int> data);
